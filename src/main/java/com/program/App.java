e1bebd336a1fae10e61ec818142108ac752ad24c
Vithun V <vithun.vdk@gmail.com>
2022-05-04 12:32:09 -0400

    maven changes

---
+++ src/main/java/com/program/App.java
@@ -0,0 +1,17 @@
+package com.program;
+
+import org.json.JSONArray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JSONArray jsonArray = new JSONArray("[]");
+        System.out.println(jsonArray.length());
+        
+    }
+}
